1f754310574f26eb52572812f8d02b576bcf2ce9
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-24 23:22:20 +0530

    server: express types created

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,16 @@
+import type { IUser } from "../models/user.model.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        email: string;
+        role: IUser["role"];
+      };
+      clientIp?: string;
+    }
+  }
+}
+
+export {};
